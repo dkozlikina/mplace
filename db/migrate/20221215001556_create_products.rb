06ccf384f79d871b4ed888c30e20ebe1d2ef9cc8
имя фамилияДаша <dkozlikina@gmail.com>
2022-12-18 15:30:27 +0300

    add product.info

--- db/migrate/20221215001556_create_products.rb
+++ db/migrate/20221215001556_create_products.rb
@@ -4,7 +4,6 @@ class CreateProducts < ActiveRecord::Migration[7.0]
       t.string :name
       t.float :price
       t.string :picture
-      t.text :info
 
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_001556) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_062524) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.float "price"
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "info"
   end
 
   create_table "users", force: :cascade do |t|
