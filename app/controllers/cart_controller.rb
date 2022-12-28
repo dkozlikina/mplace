9246e15653024fb372c45f7a91757f9497c0ac1b
имя фамилияДаша <dkozlikina@gmail.com>
2022-12-28 04:04:52 +0300

    fix product.show

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,12 @@
 class CartController < ApplicationController
   def show
     @cart = current_user.cart
+
+    ans = 0
+    current_user.cart.each  do | x |
+      ans += x.price * x.amount
+    end
+    @cost = ans
   end
 
   def add
@@ -12,6 +18,12 @@ class CartController < ApplicationController
 
   def delete
     Cart.find_by(product_id: params[:id]).delete
+  end
 
+  def cost
+    ans = 0
+    current_user.cart.each  do | x |
+      ans += x.price * x.amount
+    end
   end
 end
